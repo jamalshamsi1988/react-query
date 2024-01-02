7d3726cde9a085de7e52f0a4d31a9d202bad1959
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2024-01-03 00:36:54 +0330

    fetch data with axios and useQuery

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,19 +1,21 @@
 import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const HomePage = () => {
-    const fetchUsers= async ()=>{
-        const res= await fetch("https://jsonplaceholder.typicode.com/users");
-        const data= await res.json();
-        return data;
+    // const fetchUsers= async ()=>{
+    //     const res= await fetch("https://jsonplaceholder.typicode.com/users");
+    //     const data= await res.json();
+    //     return data;
         
-    }
+    // }
+    const fetchUsers = ()=> {return axios.get("https://jsonplaceholder.typicode.com/users");}
    
     const {data,isLoading}=useQuery({queryKey: ['repoData'],queryFn:fetchUsers});
     if(isLoading) return <h1>Loading ...</h1>
   return (
     <div>
      {
-        data.map(i => <h3 key={i.id}>{i.name}</h3>)
+        data.data.map(i => <h3 key={i.id}>{i.name}</h3>)
      }
     </div>
   )
